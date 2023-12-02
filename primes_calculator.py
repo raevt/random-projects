1f70ef627306cf61b704f2fc1de089da5799b2b0
Rae Adimer <137109904+raevt@users.noreply.github.com>
2023-12-02 00:14:04 -0500

    optimize

--- primes_calculator.py
+++ primes_calculator.py
@@ -1,13 +1,19 @@
 """
     Rae Adimer
 
-    Calculates primes between two numbers, minimum and maximum inclusive.
+    Calculates primes between two numbers, minimum and maximum inclusive. I'm not entirely sure if this works 100% of the time.
 
     It does this by multiplying together all numbers that could multiply to a number in the given range, creating a set of numbers that are not primes. It then iterates through each number in the provided range, checking whether it is in the set. If not, it is a prime number.
 
     The key to optimizing this is properly defining the numbers that could multiply to something in the range, and not repeating operations.
 
     My attempt at this is a sort of two-step approach, with an outer for-loop and nested, inner for-loop. The goal is to use relatively-efficient operations in defining the loop's start and end points to only multiply numbers that could multiply to something in the provided range.
+
+    The inner for-loop is pretty clear to optimize, I think. I limit the starting point to the smallest number which multiplies with i (the outer loop iterable) to the minimum, and the ending point to the smallest number which multiplies with i to the maximum.
+    
+    The outer for-loop is a bit less clear. I believe the starting point of the rounded-up square root of the minimum is optimal: to get to the minimum, it would need to multiply with itself or greater. And because the inner for-loop sets its starting point at the minimum divided by i, that is what it produces (save for the integer rounding aspect). 
+    
+    The outer for-loop's ending point is the maximum divided by 2; anything above that would multiply with 2 to be greater than the maximum. I'm relatively certain this isn't optimal.
 """
 import math
 import time
@@ -16,12 +22,12 @@ def gen_not_primes(maximum, minimum):
     not_primes = set()
     fail_counter = 0
     # Starts with the rounded-up square root of the minimum (or 2, if the minimum is less than 2)
-    # The rounded-up square root is the smallest number which can multiply with a number from the nested for-loop to something in the provided range...I think.
+    # This, I think, is the smallest number which can multiply with a number from the nested for-loop to something in the provided range.
     starting_point = math.ceil(minimum**(1/2))
     if starting_point < 2:
         starting_point = 2
-    for i in range(starting_point, maximum // 2):
-        for n in range((minimum // i), (maximum // i) + 1):
+    for i in range(starting_point, maximum // 2 + 1):
+        for n in range(minimum // i, (maximum // i) + 1): # Limits the starting point to the smallest number that multiplies with i to the minimum, and the ending point to the smallest number that multiplies with i to the maximum.
             if (i * n) <= maximum and (i * n) >= minimum and (i * n) not in not_primes:
                 not_primes.add(i * n)
             else:
@@ -45,7 +51,7 @@ def main():
     ts = time.time()
     print(gen_primes(gen_not_primes(maximum, minimum), minimum, maximum))
     diff = time.time() - ts
-    print(diff)
+    print("Time to solve: " + str(diff))
 
 if __name__ == "__main__":
     main()
